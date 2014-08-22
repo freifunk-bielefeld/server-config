d09078ed51c2e2b9db4610dee0ddd99236351d71
Moritz Warning <moritzwarning@web.de>
2014-08-22 14:30:29 +0200

    remove unnecessary entries

--- print_map.sh
+++ print_map.sh
@@ -2,16 +2,8 @@
 
 #Print out local connection data for map creation
 
-version="$(uci get -q freifunk.@settings[0].version 2> /dev/null)"
-hostname="$(uci get -q system.@system[0].hostname 2> /dev/null)"
-geo="$(uci get -q system.@system[0].geo 2> /dev/null)"
 
 echo -n "{"
-
-[ -n "$geo" ] && echo -n "\"geo\" : \"$geo\", "
-[ -n "$hostname" -a "$hostname" != "OpenWrt" ] && echo -n "\"name\" : \"$hostname\", "
-[ -n "$version" ] && echo -n "\"firmware\" : \"ffbi-$version\", "
-
 echo -n "\"links\" : ["
 
 printLink() { echo -n "{ \"smac\" : \"$(cat /sys/class/net/$3/address)\", \"dmac\" : \"$1\", \"qual\" : $2 }"; }
@@ -25,5 +17,5 @@ for entry in $(cat /sys/kernel/debug/batman_adv/bat0/originators |  tr '\t/[]()'
 done
 
 echo -n '], '
-cat /sys/kernel/debug/batman_adv/bat0/transtable_local | tr '\t/[]()' ' ' | awk 'BEGIN{ c=0; } { if($1 == "*" && $4 ~ /^[NW\.]+$/) c++;} END{ printf("\"clientcount\" : %d", c);}'
+echo -n "\"clientcount\" : 0"
 echo -n '}'
