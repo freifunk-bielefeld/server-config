eb868c8753b7af8ac069835b9f4ae9bfa7add697
Moritz Warning <moritzwarning@web.de>
2014-09-22 16:33:03 +0200

    fix gateway service display

--- print_service.sh
+++ print_service.sh
@@ -4,4 +4,4 @@ addr() {
 	ip -$1 address show dev $2 2> /dev/null | tr '/' ' '| awk '/inet/{print($2); exit(0);}'
 }
 
-echo -n "{\"type\" : \"gateway\", \"addr\" : \"$(addr 4 bat0)\"}"
+echo -n "{\"link\" : \"$(addr 4 bat0)\", \"label\" : \"Freifunk Gateway VPN1\"}"
