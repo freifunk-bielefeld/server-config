127aa381db4624f9aec510bc8ce2f3ffe794e5f9
Moritz Warning <moritzwarning@web.de>
2014-10-15 01:54:50 +0200

    make program positions relative

--- update.sh
+++ update.sh
@@ -3,10 +3,10 @@
 #This script is called every 5 minutes via crond
 
 #announce own piece of map information
-/root/print_map.sh | alfred -s 64
+./print_map.sh | alfred -s 64
 
 #announce own gateway service
-/root/print_service.sh | alfred -s 91
+./print_service.sh | alfred -s 91
 
 #collect all map pieces
 alfred -r 64 > /root/maps.txt
@@ -15,10 +15,10 @@ alfred -r 64 > /root/maps.txt
 alfred -r 91 > /root/services.txt
 
 #create map data
-/root/ffmap-backend.py -m /root/maps.txt -a /root/aliases.json -s /root/services.json > /var/www/nodes.json
+./ffmap-backend.py -m /root/maps.txt -s /root/services.txt -a /root/aliases.json > /var/www/nodes.json
 
 #update nodes/clients/gateways counter
-/root/counter_update.py '/var/www/map.freifunk-bielefeld.de/nodes.json' '/var/www/freifunk-bielefeld.de/counter.svg'
+./counter_update.py '/var/www/map.freifunk-bielefeld.de/nodes.json' '/var/www/freifunk-bielefeld.de/counter.svg'
 
 #update FF-Internal status page
-/root/status_page_create.sh '/var/www/vpn.ffbi/index.html'
+./status_page_create.sh '/var/www/vpn.ffbi/index.html'
