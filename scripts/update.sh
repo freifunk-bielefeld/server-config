85ca34153fc866b46b051d8088e27d5c3ca5788a
Moritz Warning <moritzwarning@web.de>
2014-10-15 16:32:53 +0200

    reorganize content

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,6 +2,8 @@
 
 #This script is called every 5 minutes via crond
 
+cd /root/scripts/
+
 #announce own piece of map information
 ./print_map.sh | alfred -s 64
 
@@ -9,13 +11,13 @@
 ./print_service.sh | alfred -s 91
 
 #collect all map pieces
-alfred -r 64 > /root/maps.txt
+alfred -r 64 > /tmp/maps.txt
 
 #create map data
-./ffmap-backend.py -m /root/maps.txt -a /root/aliases.json > /var/www/nodes.json
+./ffmap-backend.py -m /tmp/maps.txt -a ./aliases.json > /var/www/map/nodes.json
 
 #update nodes/clients/gateways counter
-./counter_update.py '/var/www/nodes.json' '/var/www/counter_image.svg'
+./counter_update.py '/var/www/map/nodes.json' '/var/www/counter/counter_image.svg'
 
 #update FF-Internal status page
-./status_page_create.sh '/var/www/index.html'
+./status_page_create.sh '/var/www/status/index.html'
