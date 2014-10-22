3995472c7ce50baf814f8ec755bb41330935e90a
Moritz Warning <moritzwarning@web.de>
2014-10-22 21:12:26 +0200

    use local address and hostname

--- scripts/print_service.sh
+++ scripts/print_service.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 addr() {
-	ip -$1 address show dev $2 2> /dev/null | tr '/' ' '| awk '/inet/{print($2); exit(0);}'
+	ip -$1 address show dev $2 2> /dev/null | tr '/' ' '| awk '/inet/{if($2 ~ /^fdef/) { print($2); exit(0);} }'
 }
 
-echo -n "{\"link\" : \"http://[$(addr 6 bat0)]/index.html\", \"label\" : \"Freifunk Gateway VPN1\"}"
+echo -n "{\"link\" : \"http://[$(addr 6 bat0)]/index.html\", \"label\" : \"Freifunk Gateway $(hostname)\"}"
