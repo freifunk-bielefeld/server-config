942e78413f89efffa002c1bfd4523413a26c8275
Moritz Warning <moritzwarning@web.de>
2015-04-30 23:34:30 +0200

    fix alfred build because of missing libcap development files

--- setup_server.sh
+++ setup_server.sh
@@ -142,7 +142,7 @@ fi
 	VERSION=2015.0
 
 	echo "(I) Install batman-adv, batctl and alfred ($VERSION)."
-	apt-get install --assume-yes wget build-essential linux-headers-$(uname -r) pkg-config libnl-3-dev libjson-c-dev git
+	apt-get install --assume-yes wget build-essential linux-headers-$(uname -r) pkg-config libnl-3-dev libjson-c-dev git libcap-dev
 
 	#install batman-adv
 	wget --no-check-certificate http://downloads.open-mesh.org/batman/releases/batman-adv-$VERSION/batman-adv-$VERSION.tar.gz
