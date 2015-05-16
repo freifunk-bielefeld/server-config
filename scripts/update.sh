89a521e6b1abcd05f13739e29b2ff6ee045feda2
Moritz Warning <moritzwarning@web.de>
2015-05-16 18:13:32 +0200

    set 'vpn' value to true, a server is not connected by wifi

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,7 +12,6 @@ geo=""
 name="$(hostname)"
 firmware="server"
 community="ulm"
-vpn="true"
 webserver="true" #start webserver, create map/status/status page
 gateway="false" #start OpenVPN, bind, tayga, radvd
 
@@ -113,6 +112,8 @@ fi
 
 #announce map information via alfred
 {
+	vpn="true"
+
 	echo -n "{"
 
 	[ -n "$geo" ] && echo -n "\"geo\" : \"$geo\", "
@@ -121,7 +122,6 @@ fi
 	[ -n "$community" ] && echo -n "\"community\" : \"$community\", "
 	[ -n "$vpn" ] && echo -n "\"vpn\" : $vpn, "
 	[ -n "$gateway" ] && echo -n "\"gateway\" : $gateway, "
-	[ -n "$vpn" ] && echo -n "\"vpn\" : $vpn, "
 
 	echo -n "\"links\" : ["
 
