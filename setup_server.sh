9bff54637ca4c3bdb0893e57ff800aed51caf639
Moritz Warning <moritzwarning@web.de>
2015-08-12 22:10:29 +0200

    use random MAC address to identify server
    (cherry picked from commit e9abd5f9f9e31a01e423b6ef79a81c3b75ca5461)

--- setup_server.sh
+++ setup_server.sh
@@ -77,14 +77,8 @@ ula_addr() {
 	echo "${prefix%%::*}:${mac%?}"
 }
 
-get_mac() {
-	local mac="$(cat /sys/class/net/$1/address)" a
-
-	# translate to local administered mac
-	a=${mac%%:*} #cut out first hex
-	a=$((0x$a ^ 2)) #invert second least significant bit
-	a=`printf '%02x\n' $a` #convert back to hex
-	echo "$a:${mac#*:}" #reassemble mac
+random_mac() {
+	echo -n 02; dd bs=1 count=5 if=/dev/urandom 2>/dev/null | hexdump -v -e '/1 ":%02x"'
 }
 
 if ! ip addr list dev $wan_iface &> /dev/null; then
@@ -92,7 +86,7 @@ if ! ip addr list dev $wan_iface &> /dev/null; then
 	exit
 fi
 
-mac_addr="$(get_mac $wan_iface)"
+mac_addr="$(random_mac)"
 ip_addr="$(ula_addr $ff_prefix $mac_addr)"
 
 if [ -z "$mac_addr" -o -z "$ip_addr" ]; then
