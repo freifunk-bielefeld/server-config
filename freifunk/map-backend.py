6ade7f4d679645bcc4da06a17a6e8522cb57d5fe
Moritz Warning <moritzwarning@web.de>
2018-12-07 23:22:26 +0100

    remove default uptime

--- freifunk/map-backend.py
+++ freifunk/map-backend.py
@@ -269,7 +269,7 @@ class Node:
         if uptime:
             uptime = fmt_time(datetime.datetime.utcnow() - datetime.timedelta(seconds=int(uptime)))
         else:
-            uptime = '1970-01-01T00:00:00'
+            uptime = ''
 
         obj = {
             'location': {},
